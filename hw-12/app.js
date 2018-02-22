236ec456b0d55df667e49e21c454c0a828c8ce0c
GTA <grant.aguinaldo@gmail.com>
2018-02-22 15:22:56 -0800

    ...more work.

---
+++ hw-12/app.js
@@ -0,0 +1,51 @@
+// Link HTMl to external app.js file. (Complete)
+
+// Import data.js to app.js file.
+
+// Create a script that dynamically creates a table for each UFO sighting.
+// The table should have a column for date/time, city, state, country, shape, and the comments.
+
+// Create an input field in the HTML that, when populated, will dynamically display the with the data on the screen.
+
+// Need to figure out how to dynamically generate a table.
+// Need to figure out how to style a table using Bootstrap and CSS.
+
+
+
+var tbody = document.querySelector("tbody");
+var dateInput = document.querySelector("#date");
+var searchBtn = document.querySelector("#search");
+
+searchBtn.addEventListener("click", handleSearchButtonClick);
+
+// Set filteredUfoData to data initially
+var filteredUfoData = data;
+
+
+
+var dateSelect = '1/1/2010'
+var shapeSelect = 'triangle'
+var citySelect = 'los angeles'
+var countrySelect = 'usa'
+var stateSelect = 'ca'
+
+for (var i = 0; i < dataSet.length; i++){
+
+    if (dataSet[i].shape === shapeSelect) {
+        console.log(dataSet[i].comments);
+    }
+}
+
+// Next step, print to HTML page instead of console using inner HTML or another method.
+
+
+/*
+    datetime: "1/1/2010",
+    city: "el cajon",
+    state: "ca",
+    country: "us",
+    shape: "triangle",
+    durationMinutes: "12 minutes",
+    comments: "3 Red objects hovering over El Cajon CA"
+
+*/
