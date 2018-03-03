78f55cb6e757dd3201a423dbe95b9f7e8e1fe9ab
GTA <grant.aguinaldo@gmail.com>
2018-03-02 17:51:19 -0800

    API completed.

--- hw-13/app.py
+++ hw-13/app.py
@@ -1,26 +1,44 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker, Query
+from flask import Flask, render_template, jsonify
+from database import otuOutput, nameOutput, washFreq, metaOutput
 
-engine = create_engine('sqlite:///DataSets/belly_button_biodiversity.sqlite', convert_unicode=True, echo=False)
-Base = declarative_base()
-Base.metadata.reflect(engine)
+app = Flask(__name__)
 
-# https://stackoverflow.com/questions/17652937/how-to-build-a-flask-application-around-an-already-existing-database
-
-
-class Otu(Base):
-    __table__ = Base.metadata.tables['otu']
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-class Samples(Base):
-    __table__ = Base.metadata.tables['samples']
+@app.route('/api/v1/otu')
+def otu():
+
+    otuOutputList = otuOutput()
+
+    return (jsonify(otuOutputList))
 
 
-class SamplesMetadata(Base):
-    __table__ = Base.metadata.tables['samples_metadata']
+@app.route('/api/v1/names')
+def names():
+
+    sampleNameList = nameOutput()
+
+    return (jsonify(sampleNameList))
 
 
-# bb_database = scoped_session(sessionmaker(bind=engine))
-# for item in bb_database.query(Samples.BB_940):
-#     print(item)
+@app.route('/api/v1/wfreq/<sample>')
+def wfreq(sample):
+
+    washOutputList = washFreq(sample)
+
+    return jsonify(washOutputList)
+
+
+@app.route('/api/v1/metadata/<sample>')
+def meta(sample):
+
+    dataDict = metaOutput(sample)
+
+    return jsonify(dataDict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
