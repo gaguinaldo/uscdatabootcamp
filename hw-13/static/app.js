9aca3595021a6f4773765ea3bf3a0499987cd84e
GTA <grant.aguinaldo@gmail.com>
2018-03-05 21:09:34 -0800

    ...

--- hw-13/static/app.js
+++ hw-13/static/app.js
@@ -1,3 +1,5 @@
+
+
 var dropDownData = d3
   .select("#selDataset")
 
@@ -14,10 +16,16 @@ Plotly.d3.json(namesUrl, function(error, response) {
 
 });
 
+
+/*
+The getData() function returns the value of the dropdown list and makse an API call to the respective end point to fetch the metadata for the sample.
+Once API call is made, the meta data is appended to the 'ul' element.
+The function then makes another API call to obtain the data needed for the pie chart, which represents the distrubution of bacteria in the given sample.
+Finally, the data is plotted and the pie chart is appended to the DOM.
+*/
 function getData() {
 
         sampleValue = document.getElementById("selDataset").value;
-        console.log(sampleValue)
 
         var endPoint = '/api/v1/metadata/' + sampleValue.split('_')[1]
 
@@ -34,31 +42,58 @@ function getData() {
             })  //Closes Plotly.d3.json(endPoint)
 
         var endPointSampleData = '/api/v1/samples/' + sampleValue
-        console.log(endPointSampleData)
 
         Plotly.d3.json(endPointSampleData, function(error, response) {
 
             if (error) return console.warn(error);
-            console.log(response['otu_ids'])
-            console.log(response['sample_values'])
 
-            function graphData(){
+            function graphPieData(){
 
-                var data = [{
+                var dataPie = [{
                     values: response['sample_values'],
                     labels: response['otu_ids'],
                     type: 'pie'
                 }];
 
-                var layout = {
+                var layoutPie = {
                     height: 600,
                     width: 800
                 };
 
-            Plotly.plot('pie', data, layout);
+            Plotly.plot('pie', dataPie, layoutPie);
             }
 
-        graphData()
+            function graphBubbleData(){
+                sizeList = []
+                for (var i = 0; i < response['sample_values'].length; i++){
+                    sizeList.push(100 * response['sample_values'][i])
+                };
+
+                var trace = {
+                    x: response['otu_ids'],
+                    y: response['sample_values'],
+                    mode: 'markers',
+                    marker: {
+                        size: [sizeList],
+                        sizemode: 'area'
+                    }
+                };
+
+                var data = [trace];
+
+                var layout = {
+                      title: 'Marker Size',
+                      showlegend: false,
+                      height: 1000,
+                      width: 1000
+                    };
+            Plotly.newPlot('bubble', data, layout);
+            }
+
+        graphPieData()
+        graphBubbleData()
 
         })
-}
+};
+
+//Need to include init function to load a data set when the page opens.  Once this is set, the getData function willl be an update.
